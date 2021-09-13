e246f16a3300ec95c10306a8c467f929fca86080
Gabriela R <64869220+gradulescuC@users.noreply.github.com>
2021-09-13 23:27:24 +0300

    Initial commit
    I added documentation and homework data

---
+++ IntroToProgramming - Organizat/Teme pentru acasa/RestrictedInstructions_CalledProgram.py
@@ -0,0 +1,11 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == 'Alina' and password =="abc123":
+    authorized = 1
+else:
+    authorized = 0
+
+if _name_ == "_main_":
+    print(username)
+    print(password)
