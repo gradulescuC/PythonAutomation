f75c2c9d067b80644eb7c50e6cfb925b06ed8413
Gabriela R <64869220+gradulescuC@users.noreply.github.com>
2021-10-15 21:20:02 +0300

    Am adaugat cateva notite de oop + rezolvarea temei

--- OOP/CalculatorClass.py
+++ OOP/CalculatorClass.py
@@ -3,7 +3,11 @@
 Calculator class used for operations
 
 -- o functie aflata in interiorul unei clase se numeste metoda
--- un constructor este un bloc de cod care ne poate ajuta sa definim atribute ale clasei valabile in toata clasa
+-- un constructor este un bloc de cod care ne poate ajuta sa definim atribute ale clasei valabile in toata clasa.
+-- sub o clasa putem ingloba toate functiile care au un scop comun, chiar daca rezultatul operatiilor lor va fi diferit
+-- o clasa se defineste folosind keyword-ul class
+-- prin conventie, numele unei clase trebuie sa inceapa cu litera mare
+
 """
 
 from OOP.Curs4 import mathOperations
@@ -22,20 +26,18 @@ class Calculator:
             return num2
 
     def max3(self, num1, num2, num3):
-        return self._max2(num1, self._max2(num2, num3))
+        return self._max2(num1, self._max2(num2, num3))  #trebuie sa punem keyword-ul self inainte de apelare, pentru ca altfel sistemul va cauta o functie max2 definita in afara clasei in loc de metoda max2
 
     def test_max3(self):
-        self.max3(5, 7, 9)
+        self.max3(5, 7, 9)  #aici am apelat functia max3, la fel folosind keyword-ul self inainte de numele metodei
 
     def max3v2(self):
         a = 2
-
         def max2v2(num1, num2):
             if num1 > num2:  # se face comparatia intre cele doua numere
                 return num1 + a  # se returneaza num1 daca e mai mare decat num2
             else:
                 return num2
-
         return self._max2(self._num1, max2v2(self._num2, self._num3))
 
     def mathOperations(self, a, b, operation):
@@ -43,10 +45,13 @@ class Calculator:
         return mathOperations(a,b,operation)
 
 
-# calculator = Calculator()
-calculator = Calculator(7,5,11)
+calculator = Calculator()  #obiectul a fost creat cu ajutorul unui constructor implicit. variabilei calculator ii atribuim o intreaga clasa. Instructiunea asta nu va merge dupa ce definim constructorul explicit, ci doar cu cel implicit, pentru ca altfel nu corespund parametrii cu argumentele
+print(calculator)
+print(calculator.test_max3())  #De ce returneaza none? Pentru ca metoda test_max3() nu are niciun return. Pentru a fixa asta adaugam la test_max3 urmatorul return: return self.max3(5,9,4)
 print("Rezultatul este",calculator)
 print("Rezultatul functiei test_max3() este: ", calculator.test_max3())  # returneaza none pentru ca metoda nu are return
 print("Rezultatul functiei max3() este: ", calculator.max3(5,6,7))
+print ("Rezultatul functiei _max2() este: ", calculator._max2(1,2))  #desi python ne lasa sa apela metoda asta, dar e o practica gresita pentru ca metoda _max2 e privata
 print("Rezultatul functiei mathOperations este:  ",calculator.mathOperations(2,4,"*"))
-print(calculator.max3v2())
+calculator = Calculator(5,7,9)  #in general unui constructor este bine sa ii dam ca parametri valori pe care le vom folosi la toate metodele
+print(calculator.max3v2())  #apelarea asta va merge doar dependent de atribuirea de la linia 57, pentru ca metoda max3v2 isi ia parametrii pe care i-am pasat in mod explicit constructorului
--- OOP/Curs4.py
+++ OOP/Curs4.py
@@ -6,17 +6,18 @@ Define four functions for basic calculator operations
 
 -- embeded function = functie predefinita in python
 """
+
 from math import sqrt
 print(max(7,5,12))
 
 def max2(num1,num2):
      if num1>num2: #se face comparatia intre cele doua numere
         return num1 #se returneaza num1 daca e mai mare decat num2
-     else:
+     else:  #acest else e optional, daca il eliminam, sistemul va subintelege ca acel return num2 e pe ramura de else, dar in cazul asta va trebui sa schimbam indentarea lui return num2 pe aceeasi linie cu if
          return num2
 
-def max3(num1,num2,num3):
-    return max2(num1,max2(num2,num3))
+def max3(num1,num2,num3): #functie cu trei argumente
+    return max2(num1,max2(num2,num3)) #aici primul parametru al functiei max2 e num2, iar al doilea parametru e maximul dintre num2 si num3
 
 print(max2(5,7))
 print(max3(7,12,5))
@@ -25,24 +26,25 @@ print("--------------------------")
 print("Nested Functions ")
 
 # Nested functions (functii care se afla in interiorul altei functii)
-def max3v2(num1,num2,num3):
-    a = 3
-    def max2v2(num1,num2):
+def max3v2(num1,num2,num3): #functie cu trei argumente
+    a = 3 #definim o variabila a in care stocam valoarea 3. E o variabila locala, nu o sa o putem folosi in alta parte in afara functiei
+    def max2v2(num1,num2): #definim functia max2v2 in interiorul functiei mx3v2
         if num1 > num2:  # se face comparatia intre cele doua numere
             return num1  # se returneaza num1 daca e mai mare decat num2
         else:
             return num2
     print(a)
-    return max2v2(num1,max2v2(num2,num3))
+    return max2v2(num1,max2v2(num2,num3)) #se returneaza rezultatul unei functii
 
 print("Numarul maxim este", max3v2(45,23,12))
 # functia max2v2 e valabila doar in interiorul functiei max3v2
-# print(a) -- Variabila a nu va fi vazuta pentru ca e declarata in interiorul functiei max3v2
+# print(a) -- Variabila a nu va fi vazuta pentru ca e declarata in interiorul functiei max3v2. Instructiunea asta va da eroare pentru ca variabila a nu e recunoscuta
+#avantajele functiilor nested e de a putea sa grupam codul astfel incat sa nu complicam codul cu instructiuni pe care le vom folosi doar o singura   data
 
 print("--------------------------")
 print("Operatii matematice")
 
-def mathOperations(a,b,operation):
+def mathOperations(a,b,operation): #functie de calcul a operatiilor matematice, cu trei argumente
     if operation == "+":
         return a + b
     elif operation == "-":
